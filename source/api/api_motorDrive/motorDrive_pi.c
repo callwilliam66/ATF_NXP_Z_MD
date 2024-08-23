b297b5fe06f04acf9f5d2864471aa96bebc18162
william <160986768+callwilliam66@users.noreply.github.com>
2024-08-23 14:27:21 +0800

    Develope_prototype_V3

--- source/api/api_motorDrive/motorDrive_pi.c
+++ source/api/api_motorDrive/motorDrive_pi.c
@@ -176,7 +176,7 @@ void pi_feed_forward(PI_REGS *piRegs)
 
 	piRegs->fUf = piRegs->fRef * piRegs->fKf;
 
-	piRegs->fURf = piRegs->fFbk * piRegs->fRKf ;
+	piRegs->fURf = piRegs->fFbk * piRegs->fRKf;
 
 	piRegs->fOutSat = piRegs->fUp + piRegs->fUi + piRegs->fUf - piRegs->fURf;
 
